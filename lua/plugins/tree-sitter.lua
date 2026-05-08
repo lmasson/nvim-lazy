aad0b19d5809a20514423894865b86500642cd9e
Louis Masson <l.masson@swissto12.ch>
2026-05-08 15:18:57 +0200

    feat: enable treesitter modules

--- lua/plugins/tree-sitter.lua
+++ lua/plugins/tree-sitter.lua
@@ -2,5 +2,44 @@ return {
     "nvim-treesitter/nvim-treesitter",
     branch = 'master',
     lazy = false,
-    build = ":TSUpdate"
+    build = ":TSUpdate",
+
+  opts = {
+    ensure_installed = {
+      "python",
+      "lua",
+      "bash",
+      "javascript",
+      "html",
+      "css",
+      "json",
+      "yaml",
+      "markdown",
+      "markdown_inline",
+    },
+
+    highlight = {
+      enable = true,
+      additional_vim_regex_highlighting = false,
+    },
+
+    indent = {
+      enable = true,
+    },
+
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = "gnn",
+        node_incremental = "grn",
+        scope_incremental = "grc",
+        node_decremental = "grm",
+      },
+    },
+  },
+
+  config = function(_, opts)
+    require("nvim-treesitter.configs").setup(opts)
+  end
 }
+
