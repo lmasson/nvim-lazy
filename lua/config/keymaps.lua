d80fe5a3827b27291d43e0a65fb4c47ab30f3f14
Louis Masson <l.masson@swissto12.ch>
2026-05-27 15:01:22 +0200

    feat: formatting keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -25,6 +25,9 @@ vim.keymap.set('n', '<leader>tl', ':tabnext<CR>', { desc = 'Next tab' })
 vim.keymap.set('n', '<leader>th', ':tabprevious<CR>', { desc = 'Previous tab' })
 vim.keymap.set('n', '<leader>tc', ':tabclose<CR>', { desc = 'Close tab' })
 
+-- Formatting
+vim.keymap.set("n", "<leader>fo", function() vim.lsp.buf.format() end)
+
 -----------------
 -- Telescope
 -----------------
